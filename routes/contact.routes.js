31fff6404573725c8e3ae6d6344fc9ca705838f3
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-07-03 01:18:05 +0100

    update app

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -3,43 +3,38 @@ const nodemailer = require('nodemailer');
 
 const { redirectLogin } = require("../middlewares/auth.middleware");
 
+const userEmail = "haikel.fazzani@zoho.com";
+const pass = 'WxC 0702106';
+
 /* GET contact page. */
-router.get('/', redirectLogin, (req, res) => {
-  if (req.session.userInfo || res.locals.userInfo) {
-    const user = req.session.userInfo || res.locals.userInfo;
-    res.render('contact', { data: user });
-  }
-});
+router.get('/', redirectLogin, (req, res) => res.render('contact', { data: userEmail }));
 
 
 router.post('/', redirectLogin, (req, res) => {
-  if (req.session.userInfo || res.locals.userInfo) {
 
-    const user = req.session.userInfo || res.locals.userInfo;
-    const { email, emailDest, sujet, message } = req.body;
+  const { email, emailDest, sujet, message } = req.body;
 
-    // haikel.fazzanii@outlook.fr
-    let transporter = nodemailer.createTransport({
-      service: 'zoho',
-      host: 'smtp.zoho.com',
-      port: 465,
-      secure: false,
-      auth: { user: email.trim(), pass: 'WxC 0702106' }
-    });
+  // haikel.fazzanii@outlook.fr
+  let transporter = nodemailer.createTransport({
+    service: 'zoho',
+    host: 'smtp.zoho.com',
+    port: 465,
+    secure: false,
+    auth: { user: email.trim(), pass }
+  });
 
-    let mailOptions = {
-      from: email.trim(), to: emailDest.trim(), subject: sujet.trim(), text: message.trim()
-    };
+  let mailOptions = {
+    from: email.trim(), to: emailDest.trim(), subject: sujet.trim(), text: message.trim()
+  };
 
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        res.render('contact', { msg: error, data: user });
-      } else {
-        res.render('contact', { msg: "un email a été bien envoyé", data: user });
-      }
-    });
+  transporter.sendMail(mailOptions, function (error, info) {
 
-  }
+    res.render('contact',
+      { msg: error ? error : "un email a été bien envoyé", data: userEmail });
+
+  });
+
+
 });
 
 module.exports = router;
